9dc38240edfb57adfe6e5eff455fa2c55514d8a5
lisette-18 <horne146@umn.edu>
2024-05-02 12:51:16 -0700

    Data cleaning

--- R/final.R
+++ R/final.R
@@ -5,3 +5,20 @@ library(haven)
 library(caret)
 
 #Data Import and Cleaning
+gss_2004_import_tbl <- read_sav("../data/GSS2004.sav") %>%
+  select(-where(~mean(is.na(.))>.75)) %>%
+  mutate(across(everything(), as.numeric)) %>%
+  mutate(SEX = factor(SEX,
+                      levels=c("1","2"),
+                      labels=c("Male","Female")), 
+         exclude = NA,
+         RACE = factor(RACE,
+                       levels = c("1","2","3"),
+                       labels = c("White", "Black", "Other")),
+         avg_att = rowMeans(across(c(EMPATHY1, EMPATHY2,EMPATHY3, EMPATHY4,EMPATHY5, EMPATHY6, EMPATHY7))), #lower scores is more accepting attitudes towards others
+         avg_probehav = rowMeans(across(c(GIVBLOOD, GIVHMLSS, RETCHNGE, CUTAHEAD, VOLCHRTY, GIVCHRTY, GIVSEAT, HELPAWAY, CARRIED, DIRECTNS, LOANITEM)))) %>%
+  #lower scores is more enacted pro social behavior
+  select(SEX, RACE, avg_att, avg_probehav)
+
+gss_2004_tbl <- gss_2004_import_tbl[complete.cases(gss_2004_import_tbl$avg_att, gss_2004_import_tbl$avg_probehav), ]
+
